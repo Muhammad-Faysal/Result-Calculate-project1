7844261ad730651fcdeec8bde9eec0fdd155e41a
faysal2525 <faisal_2525@outlook.com>
2024-06-06 20:19:25 +0500

    add

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 function calulateResult() {
-        let userNumber = document.getElementById('number').value
-        let calculatedPercentage = userNumber / 750 * 100;  106
-        alert(calculatedPercentage)
-        if(calculatedPercentage >= 80 && calculatedPercentage < 100  ) {
-            alert("You got A+ congrats")
-        }
-        else if(calculatedPercentage >= 70 && calculatedPercentage < 80 ) {
-            alert("You got A congrats")
-        }
-        else if(calculatedPercentage >= 60  && calculatedPercentage < 70 ) {
-            alert("You got B congrats")
-        }
-        else if (calculatedPercentage >= 50  && calculatedPercentage < 60 ) {
-            alert("You got C congrats")
-        }
-        else if (calculatedPercentage >= 40  && calculatedPercentage < 50 ) {
-            alert(" You got D congrats")
-        }
-        else {
-            alert("please enter a valid input")
-        }
-        // console.log(userNumber)
-       
-    }
+  let userNumber = document.getElementById("number").value;
+  let calculatedPercentage = (userNumber / 750) * 100;
+  106;
+
+  alert(calculatedPercentage);
+
+  if (calculatedPercentage >= 80 && calculatedPercentage < 100) {
+    // alert("Congrats Your Grade A+")
+
+    Swal.fire({
+      title: "Congrats Your Grade A+",
+      text: "That thing is still around?",
+      icon: "success",
+    });
+  } else if (calculatedPercentage >= 70 && calculatedPercentage < 80) {
+    // alert("Congrats Your Grade A")
+
+    Swal.fire({
+      title: "Congrats Your Grade A",
+      text: "That thing is still around?",
+      icon: "success",
+    });
+  } else if (calculatedPercentage >= 60 && calculatedPercentage < 70) {
+    // alert("Congrats Your Grade B")
+
+    Swal.fire({
+      title: "Congrats Your Grade B",
+      text: "That thing is still around?",
+      icon: "success",
+    });
+  } else if (calculatedPercentage >= 50 && calculatedPercentage < 60) {
+    // alert("Congrats Your Grade C")
+
+    Swal.fire({
+      title: "Congrats Your Grade C",
+      text: "That thing is still around?",
+      icon: "success",
+    });
+  } else if (calculatedPercentage >= 40 && calculatedPercentage < 50) {
+    // alert(" Congrats Your Grade D")
+
+    Swal.fire({
+      title: "Congrats Your Grade D",
+      text: "That thing is still around?",
+      icon: "success",
+    });
+  } else {
+    alert("please enter a valid input");
+  }
+  // console.log(userNumber)
+}
