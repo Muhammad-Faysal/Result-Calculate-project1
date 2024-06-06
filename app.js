4a2a45b733d9ee2cb01561c301ab83fe5f9443f8
faysal2525 <faisal_2525@outlook.com>
2024-06-06 19:25:35 +0500

    add more item

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 function calulateResult() {
         let userNumber = document.getElementById('number').value
-        let calculatedPercentage = userNumber / 750 * 100;  //106
-        console.log(calculatedPercentage)
-        if (calculatedPercentage >= 80) {
+        let calculatedPercentage = userNumber / 750 * 100;  106
+        alert(calculatedPercentage)
+        if(calculatedPercentage >= 80 && calculatedPercentage < 100  ) {
             alert("You got A+ congrats")
         }
-        else if (calculatedPercentage >= 70) {
+        else if(calculatedPercentage >= 70 && calculatedPercentage < 80 ) {
             alert("You got A congrats")
         }
-        else if (calculatedPercentage >= 60) {
+        else if(calculatedPercentage >= 60  && calculatedPercentage < 70 ) {
             alert("You got B congrats")
         }
-        else if (calculatedPercentage >= 50) {
+        else if (calculatedPercentage >= 50  && calculatedPercentage < 60 ) {
             alert("You got C congrats")
         }
-        else if (calculatedPercentage >= 40) {
+        else if (calculatedPercentage >= 40  && calculatedPercentage < 50 ) {
             alert(" You got D congrats")
         }
         else {
             alert("please enter a valid input")
         }
-        console.log(userNumber)
+        // console.log(userNumber)
        
     }
